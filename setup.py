4a3a680d619ab672202ab4c93135f5599cf17198
Steve Mulligan <steve@nonlocal.ca>
2020-10-18 14:43:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Flask-Ask
 Easy Alexa Skills Kit integration for Flask
 """
 from setuptools import setup
-from pip.req import parse_requirements
+from pip._internal.req import parse_requirements
 
 setup(
     name='Flask-Ask',
